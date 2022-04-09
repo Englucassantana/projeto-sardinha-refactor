31fa63aa1759af6d83012643d7198f62f07baf13
Lucas Santana <eng.lucassantana@gmail.com>
2022-04-09 16:22:46 -0300

    Mostrar apenas pair USDT

--- js/binance-request.js
+++ js/binance-request.js
@@ -1,16 +1,17 @@
 let listPair;
 let arrayPair = [];
 let url = 'https://api.binance.com/api/v3/ticker/price';
-let xhr = new XMLHttpRequest();
 
-xhr.open('GET',url,true);
-xhr.addEventListener('load',()=>{
-  listPair = JSON.parse(xhr.responseText);
-  listPair.forEach(element => {
-    arrayPair.push(element['symbol']);
-  });
-  $("#pair-input").autocomplete({
-    source:arrayPair
-  });
-})
-xhr.send(null);
+$.get(url, null,
+  function (data, textStatus) {
+    if(textStatus == 'success'){
+      listPair = data;
+      listPair.forEach(element => {
+        if(element['symbol'].indexOf('USDT') != -1){
+          arrayPair.push(element['symbol']);
+        }
+      });
+    }
+  },
+  "JSON"
+);
